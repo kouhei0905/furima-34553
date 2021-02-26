308e318c731975d1a55167362e11f04d3ccdd119
kouhei0905 <mettoooo.1031@gmail.com>
2021-02-26 18:02:20 +0900

    usersテーブルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :kana_firstname, presence: true
+  validates :kana_lastname, presence: true
+  validates :birthday, presence: true
+
+end
