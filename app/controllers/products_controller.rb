abb440852919c6e671d45f6a42e6778fb6bb7e8b
kouhei0905 <mettoooo.1031@gmail.com>
2021-03-01 12:26:24 +0900

    コントローラ名変更　アソシエーション追加

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,20 @@
+class ProductsController < ApplicationController
+  def index
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+  end
+
+
+  private
+  def product_params
+    params.require(:product).permit(:productname, :description, :category_id, :status_id, :burden_id, :days_id, :price, :image ).merge(user_id: current_user.id)
+  end
+
+end
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  has_one_attached :image
+
+
+  with_options presence: true do
+    validates :productname
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :days_id
+    validates :price 
 end
