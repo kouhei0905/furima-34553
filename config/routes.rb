f96a79b2f52d97f1a53bd9f0793d44d9deb2ffc5
kouhei0905 <mettoooo.1031@gmail.com>
2021-03-03 11:29:21 +0900

    画像をクリックすると商品詳細画面へ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "products#index"
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:index, :new, :create, :show]
 
   devise_for :users
 end
