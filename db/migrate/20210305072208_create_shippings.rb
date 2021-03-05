85872c17a02aa83f13809a0c46ec80efeb008dc0
kouhei0905 <mettoooo.1031@gmail.com>
2021-03-05 17:59:05 +0900

    フォームの実装

--- db/migrate/20210305072208_create_shippings.rb
+++ db/migrate/20210305072208_create_shippings.rb
@@ -1,7 +1,13 @@
 class CreateShippings < ActiveRecord::Migration[6.0]
   def change
     create_table :shippings do |t|
-
+      t.integer       :prefecture_id,    null: false
+      t.string        :postalcode,       null: false
+      t.string        :municipality,     null: false
+      t.string        :addres,           null: false
+      t.string        :building
+      t.string        :phonenumber,      null: false
+      t.references    :purchase,         null: false, foreign_key: true
       t.timestamps
     end
   end
