aa2ec9ac6f7577ee80854a3789f383dc5edb44e5
kouhei0905 <mettoooo.1031@gmail.com>
2021-03-08 17:44:54 +0900

    クレジットカード機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,33 @@
 class OrdersController < ApplicationController
   def index
-    @purchase_shipping = PurchaseShipping.new
-  end
-
-  def new
+    @product = Product.find(params[:product_id])
     @purchase_shipping = PurchaseShipping.new
   end
 
   def create
-    @purchase_shipping = PurchaseShipping.new
+    @product = Product.find(params[:product_id])
+    @purchase_shipping = PurchaseShipping.new(purchase_shipping_params)
+    if @purchase_shipping.valid?
+      pay_item
+      @purchase_shipping.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_shipping_params
+    params.require(:purchase_shipping).permit( :prefecture_id, :postalcode, :municipality , :addres, :building, :phonenumber).merge(user_id: current_user.id, token: params[:token], product_id: params[:product_id])
+  end
+
+  def pay_item
+    Payjp.api_key =  ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: purchase_shipping_params[:token],
+      currency: 'jpy' 
+    )
   end
 end
--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,6 +1,7 @@
 class PurchaseShipping
   include ActiveModel::Model
-  attr_accessor :postal_code, :user_id, :product_id, :prefecture_id, :postalcode, :municipality, :addres, :building, :phonenumber, :purchase
+  attr_accessor :user_id, :product_id, :prefecture_id, :postalcode, :municipality, :addres, :building, :phonenumber ,:token
+  #  :product_id, token
 
   with_options presence: true do
     validates :prefecture_id 
@@ -8,14 +9,15 @@ class PurchaseShipping
     validates :addres 
     validates :building
     validates :phonenumber
+    validates :token
   end
 
   def save
     # 購入情報を保存し、 parchase変数に代入する
-    parchase = Parchase.create(user_id: user_id, product_id: product_id)
+    purchase = Purchase.create(user_id: user_id, product_id: product_id)
     # 住所を保存する
     # purchase_idには、変数purchaseのidと指定する
-    Address.create(prefecture_id: prefecture_id, postalcode: postalcode, municipality: municipality, addres: addres, building: building, phonenumber: phonenumber, purchase_id: purchase.id)
+    Shipping.create(prefecture_id: prefecture_id, postalcode: postalcode, municipality: municipality, addres: addres, building: building, phonenumber: phonenumber, purchase_id: purchase.id)
   end
 
 end
